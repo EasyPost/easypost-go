c715d4365e3e8193e410539039b4250244c5594d
Justin Hammond <39606064+Justintime50@users.noreply.github.com>
2020-07-02 16:37:08 -0600

    Add Tracker Examples (#22)

---
+++ examples/trackers/retrieve_all/retrieve_trackers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/EasyPost/easypost-go"
+	"os"
+)
+
+func main() {
+	apiKey := os.Getenv("EASYPOST_API_KEY")
+	if apiKey == "" {
+		fmt.Fprintln(os.Stderr, "missing API key")
+		os.Exit(1)
+		return
+	}
+	client := easypost.New(apiKey)
+
+	// Retrieve a list of trackers
+	tracker, err := client.ListTrackers(
+		&easypost.ListTrackersOptions{
+			// Options here
+		},
+	)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error retrieving trackers:", err)
+		os.Exit(1)
+		return
+	}
+
+	prettyJSON, err := json.MarshalIndent(tracker, "", "    ")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error creating JSON:", err)
+	}
+	fmt.Printf("%s\n", string(prettyJSON))
+}
