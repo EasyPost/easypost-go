dd10987e3e49b36e778af55d5ddd3564183b143d
LoganSimonsen <logan.simonsen@easypost.com>
2020-10-08 21:34:33 -0600

    removed extra parameter in example

--- examples/batches/scan_form_batch/scan_form_batch.go
+++ examples/batches/scan_form_batch/scan_form_batch.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "github.com/EasyPost/easypost-go"
-    "os"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/EasyPost/easypost-go"
 )
 
 func main() {
-    apiKey := os.Getenv("EASYPOST_API_KEY")
-    if apiKey == "" {
-        fmt.Fprintln(os.Stderr, "missing API key")
-        os.Exit(1)
-        return
-    }
-    client := easypost.New(apiKey)
+	apiKey := os.Getenv("EASYPOST_API_KEY")
+	if apiKey == "" {
+		fmt.Fprintln(os.Stderr, "missing API key")
+		os.Exit(1)
+		return
+	}
+	client := easypost.New(apiKey)
 
-    // Create a ScanForm for a Batch
-    batch, err := client.CreateBatchScanForms(
-        "batch_12345",
-        "string",
-    )
-    if err != nil {
-        fmt.Fprintln(os.Stderr, "error creating ScanForm for Batch:", err)
-        os.Exit(1)
-        return
-    }
+	// Create a ScanForm for a Batch
+	batch, err := client.CreateBatchScanForms(
+		"batch_12345",
+	)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error creating ScanForm for Batch:", err)
+		os.Exit(1)
+		return
+	}
 
-    prettyJSON, err := json.MarshalIndent(batch, "", "    ")
-    if err != nil {
-        fmt.Fprintln(os.Stderr, "error creating JSON:", err)
-        os.Exit(1)
-        return
-    }
-    fmt.Println(string(prettyJSON))
+	prettyJSON, err := json.MarshalIndent(batch, "", "    ")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error creating JSON:", err)
+		os.Exit(1)
+		return
+	}
+	fmt.Println(string(prettyJSON))
 }
