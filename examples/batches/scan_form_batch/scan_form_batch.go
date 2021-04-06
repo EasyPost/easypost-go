c7ca84e8af64bc43a126880b9a45a3efd4e94f9a
Logan Simonsen <32521559+LoganSimonsen@users.noreply.github.com>
2021-04-06 16:12:11 -0600

    Change 'GetBatchScanForms' to be 'CreateBatchScanForms' (#23)
    * corrected GetBatchScanForms to be CreateBatchScanForms as a POST instead of a GET

---
+++ examples/batches/scan_form_batch/scan_form_batch.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/EasyPost/easypost-go"
+	"os"
+)
+
+func main() {
+	apiKey := os.Getenv("EASYPOST_API_KEY")
+	if apiKey == "" {
+		fmt.Fprintln(os.Stderr, "missing API key")
+		os.Exit(1)
+		return
+	}
+	client := easypost.New(apiKey)
+
+	// Create a ScanForm for a Batch
+	batch, err := client.CreateBatchScanForms(
+		"batch_123",
+		"batch_456",
+	)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error creating ScanForm for Batch:", err)
+		os.Exit(1)
+		return
+	}
+
+	prettyJSON, err := json.MarshalIndent(batch, "", "    ")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error creating JSON:", err)
+		os.Exit(1)
+		return
+	}
+	fmt.Println(string(prettyJSON))
+}
