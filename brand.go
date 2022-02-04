575d4d194d7e39b2ee2ca0521316db717af27707
jchen293 <jchen@easypost.com>
2022-02-04 11:53:25 -0500

    add struct comment.

--- brand.go
+++ brand.go
@@ -1,5 +1,6 @@
 package easypost
 
+// Brand contains data for a user brand
 type Brand struct {
 	BackgroundColor string `json:"background_color,omitempty"`
 	Color           string `json:"color,omitempty"`
