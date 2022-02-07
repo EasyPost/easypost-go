4ee4f4eba71a9fccc61dda561e9c12b6f1931f23
jchen293 <jchen@easypost.com>
2022-02-07 17:21:23 -0500

    update code example for lowest_rate.

--- examples/shipments/lowest_rate/lowest_rate.go
+++ examples/shipments/lowest_rate/lowest_rate.go
@@ -17,101 +17,61 @@ func main() {
 	}
 	client := easypost.New(apiKey)
 
-	// Create and verify addresses
-	toAddress, err := client.CreateAddress(
-		&easypost.Address{
-			Name:    "Bugs Bunny",
-			Street1: "4000 Warner Blvd",
-			City:    "Burbank",
-			State:   "CA",
-			Zip:     "91522",
-			Phone:   "8018982020",
-		},
-		&easypost.CreateAddressOptions{Verify: []string{"delivery"}},
-	)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "error creating to address:", err)
-		os.Exit(1)
-		return
+	toAddress := &easypost.Address{
+		Name:    "Bugs Bunny",
+		Street1: "4000 Warner Blvd",
+		City:    "Burbank",
+		State:   "CA",
+		Zip:     "91522",
+		Phone:   "8018982020",
 	}
 
-	fromAddr, err := client.CreateAddress(
-		&easypost.Address{
-			Company: "EasyPost",
-			Street1: "One Montgomery St",
-			Street2: "Ste 400",
-			City:    "San Francisco",
-			State:   "CA",
-			Zip:     "94104",
-			Phone:   "415-555-1212",
-		},
-		&easypost.CreateAddressOptions{Verify: []string{"delivery"}},
-	)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "error creating from address:", err)
-		os.Exit(1)
-		return
+	fromAddress := &easypost.Address{
+		Company: "EasyPost",
+		Street1: "One Montgomery St",
+		Street2: "Ste 400",
+		City:    "San Francisco",
+		State:   "CA",
+		Zip:     "94104",
+		Phone:   "415-555-1212",
 	}
 
-	// Create a parcel
-	parcel, err := client.CreateParcel(
-		&easypost.Parcel{
-			Length: 10.2,
-			Width:  7.8,
-			Height: 4.3,
-			Weight: 21.2,
-		},
-	)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "error creating parcel:", err)
-		os.Exit(1)
-		return
+	parcel := &easypost.Parcel{
+		Length: 10.2,
+		Width:  7.8,
+		Height: 4.3,
+		Weight: 21.2,
 	}
 
-	// Create a customs_info form for international shipments
-	customsItem, err := client.CreateCustomsItem(
-		&easypost.CustomsItem{
-			Description:    "EasyPost t-shirts",
-			HSTariffNumber: "123456",
-			OriginCountry:  "US",
-			Quantity:       2,
-			Value:          96.27,
-			Weight:         21.1,
-		},
-	)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "error creating customs item:", err)
-		os.Exit(1)
-		return
+	customsItem := &easypost.CustomsItem{
+		Description:    "EasyPost t-shirts",
+		HSTariffNumber: "123456",
+		OriginCountry:  "US",
+		Quantity:       2,
+		Value:          96.27,
+		Weight:         21.1,
 	}
 
-	customsInfo, err := client.CreateCustomsInfo(
-		&easypost.CustomsInfo{
-			CustomsCertify:    true,
-			CustomsSigner:     "Wile E. Coyote",
-			ContentsType:      "gift",
-			EELPFC:            "NOEEI 30.37(a)",
-			NonDeliveryOption: "return",
-			RestrictionType:   "none",
-			CustomsItems:      []*easypost.CustomsItem{customsItem},
-		},
-	)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "error creating customs info:", err)
-		os.Exit(1)
-		return
+	customsInfo := &easypost.CustomsInfo{
+		CustomsCertify:    true,
+		CustomsSigner:     "Wile E. Coyote",
+		ContentsType:      "gift",
+		EELPFC:            "NOEEI 30.37(a)",
+		NonDeliveryOption: "return",
+		RestrictionType:   "none",
+		CustomsItems:      []*easypost.CustomsItem{customsItem},
 	}
 
 	// Create the shipment
 	shipment, err := client.CreateShipment(
 		&easypost.Shipment{
 			ToAddress:   toAddress,
-			FromAddress: fromAddr,
+			FromAddress: fromAddress,
 			Parcel:      parcel,
-			// CarrierAccountIDs: []string{"ca_123",},
 			CustomsInfo: customsInfo,
 		},
 	)
+
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "error creating shipment:", err)
 		os.Exit(1)
