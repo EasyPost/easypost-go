62a93021ca62bd716e3823d71fa91c3eea5af86d
Nate Harris <nwithan8@users.noreply.github.com>
2022-07-08 14:44:14 -0600

    New billing functions (#100)
    - Remove old credit card functions
    - Add new billing functions (oh look, enums are a thing in Golang)
    - Fix type, serialization name for credit card expiration info

--- payment_method.go
+++ payment_method.go
@@ -1,30 +1,88 @@
 package easypost
 
 import (
-	"context"
 	"errors"
+	"strings"
+)
+
+type PaymentMethodPriority int64
+
+const (
+	PrimaryPaymentMethodPriority PaymentMethodPriority = iota
+	SecondaryPaymentMethodPriority
+)
+
+type PaymentMethodType int64
+
+const (
+	CreditCardPaymentType PaymentMethodType = iota
+	BankAccountPaymentType
 )
 
 type PaymentMethod struct {
-	ID                     string      `json:"id,omitempty"`
-	Object                 string      `json:"object,omitempty"`
-	PrimaryPaymentMethod   *CreditCard `json:"primary_payment_method,omitempty"`
-	SecondaryPaymentMethod *CreditCard `json:"secondary_payment_method,omitempty"`
+	ID                     string               `json:"id,omitempty"`
+	Object                 string               `json:"object,omitempty"`
+	PrimaryPaymentMethod   *PaymentMethodObject `json:"primary_payment_method,omitempty"`
+	SecondaryPaymentMethod *PaymentMethodObject `json:"secondary_payment_method,omitempty"`
 }
 
-// ListPaymentMethods returns the payment methods associated with the current user.
-func (c *Client) ListPaymentMethods() (out *PaymentMethod, err error) {
-	return c.ListPaymentMethodsWithContext(context.Background())
+type PaymentMethodObject struct {
+	BankName        string `json:"bank_name,omitempty"`   // bank account
+	Brand           string `json:"brand,omitempty"`       // credit card
+	Country         string `json:"country,omitempty"`     // bank account
+	DisabledAt      string `json:"disabled_at,omitempty"` // both
+	ExpirationMonth int    `json:"exp_month,omitempty"`   // credit card
+	ExpirationYear  int    `json:"exp_year,omitempty"`    // credit card
+	ID              string `json:"id,omitempty"`          // both
+	Last4           string `json:"last4,omitempty"`       // both
+	Name            string `json:"name,omitempty"`        // credit card
+	Object          string `json:"object,omitempty"`      // both
+	Verified        bool   `json:"verified,omitempty"`    // bank account
 }
 
-// ListPaymentMethodsWithContext performs the same as ListPaymentMethods, but
-// allows specifying a context that can interrupt the request.
-func (c *Client) ListPaymentMethodsWithContext(ctx context.Context) (out *PaymentMethod, err error) {
-	err = c.get(ctx, "payment_methods", &out)
+// getPaymentMethodObjectType returns the PaymentMethodType enum of a PaymentMethodObject.
+func (c *Client) getPaymentMethodObjectType(object *PaymentMethodObject) (out PaymentMethodType, err error) {
+	if strings.HasPrefix(object.ID, "card_") {
+		out = CreditCardPaymentType
+	} else if strings.HasPrefix(object.ID, "bank_") {
+		out = BankAccountPaymentType
+	} else {
+		return out, errors.New("no matching payment method type found")
+	}
+	return
+}
 
-	if out.ID == "" {
-		return out, errors.New("billing has not been setup for this user. please add a payment method")
+// getPaymentMethodEndpoint returns the associated endpoint for a PaymentMethodObject based on its type.
+func (c *Client) getPaymentMethodEndpoint(object *PaymentMethodObject) (out string, err error) {
+	paymentMethodType, _ := c.getPaymentMethodObjectType(object)
+	switch paymentMethodType {
+	case CreditCardPaymentType:
+		out = "credit_cards"
+	case BankAccountPaymentType:
+		out = "bank_accounts"
+	default:
+		return out, errors.New("no matching payment method type found")
+	}
+	return
+}
+
+// getPaymentMethodByPriority returns the PaymentMethodObject associated with the given PaymentMethodPriority.
+func (c *Client) getPaymentMethodByPriority(priority PaymentMethodPriority) (out *PaymentMethodObject, err error) {
+	paymentMethods, err := c.RetrievePaymentMethods()
+	if err != nil {
+		return out, err
 	}
 
-	return
+	switch priority {
+	case PrimaryPaymentMethodPriority:
+		out = paymentMethods.PrimaryPaymentMethod
+	case SecondaryPaymentMethodPriority:
+		out = paymentMethods.SecondaryPaymentMethod
+	}
+
+	if out == nil {
+		return out, errors.New("the chosen payment method is not set up yet")
+	}
+
+	return out, nil
 }
