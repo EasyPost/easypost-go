b9aa725a5712ed0b35db118beff8bc0c58ca4443
Nate Harris <nwithan8@users.noreply.github.com>
2023-10-04 11:03:01 -0600

    - Add `GetAPIKeysForUser` function to API Key service (#200)

--- tests/api_key_test.go
+++ tests/api_key_test.go
@@ -1,5 +1,10 @@
 package easypost_test
 
+import (
+	"github.com/EasyPost/easypost-go/v3"
+	"reflect"
+)
+
 func (c *ClientTests) TestApiKeysAll() {
 	client := c.ProdClient()
 	assert, require := c.Assert(), c.Require()
@@ -8,4 +13,30 @@ func (c *ClientTests) TestApiKeysAll() {
 	require.NoError(err)
 
 	assert.NotNil(apiKeys)
+}
+
+func (c *ClientTests) TestApiKeysForParentUser() {
+	client := c.ProdClient()
+	assert, require := c.Assert(), c.Require()
+
+	authenticatedUser, err := client.RetrieveMe()
+	require.NoError(err)
+
+	apiKeys, err := client.GetAPIKeysForUser(authenticatedUser.ID)
+	require.NoError(err)
+
+	assert.NotNil(apiKeys)
+}
+
+func (c *ClientTests) TestApiKeysForChildUser() {
+	client := c.ProdClient()
+	assert, require := c.Assert(), c.Require()
+
+	fakeChildUserId := "user_123456789"
+
+	apiKeys, err := client.GetAPIKeysForUser(fakeChildUserId)
+	require.Error(err)
+
+	assert.Nil(apiKeys)
+	assert.Equal(reflect.TypeOf(&easypost.FilteringError{}), reflect.TypeOf(err))
 }
